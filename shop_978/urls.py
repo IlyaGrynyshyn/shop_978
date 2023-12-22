2526848d481e4a96b22ab9578f0608a463a20278
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-22 20:39:20 +0200

    rename project name to shop_978

--- shop_978/urls.py
+++ shop_978/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mainapp project.
+URL configuration for shop_978 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
