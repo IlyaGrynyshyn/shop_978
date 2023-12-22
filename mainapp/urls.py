05636d05ea4a70a3659a8a8a00174b4003291bb1
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-22 21:49:56 +0200

    relize baselistview for mainpage & url

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from mainapp.views import BaseListView
 
 urlpatterns = [
+    path("", BaseListView.as_view(), name="home")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mainapp.models import Product, TopCategory, Category
+
+
+class BaseListView(ListView):
+    model = Product
+    template_name = "mainapp/mainpage.html"
+    context_object_name = 'products'
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['top_category'] = TopCategory.objects.all()
+        context['categories'] = Category.objects.filter(top_category=None)
+        return context
