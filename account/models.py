941dc9ae01d2972ac9c0488e682ee20d0121704a
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-23 13:39:06 +0200

    Implement model Customer

--- account/models.py
+++ account/models.py
@@ -1,27 +1,6 @@
-from django.contrib.auth.models import PermissionsMixin, AbstractUser
-from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Customer(AbstractUser):
-    username = models.CharField(
-        "username",
-        max_length=150,
-        unique=True,
-        help_text=(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[AbstractUser.username_validator],
-        error_messages={
-            "unique": ("A user with that username already exists."),
-        },
-        null=True,
-        blank=True
-    )
-    email = models.EmailField("email address", unique=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
     class Meta:
         verbose_name = "Customer"
-        verbose_name_plural = "Customer"
+        verbose_name_plural = "Customers"
