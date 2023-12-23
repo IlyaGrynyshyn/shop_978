9d8fe0ab4ae756b3fb574db29b42f68bfd5a2839
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-23 11:24:49 +0200

    relize model customer

--- account/models.py
+++ account/models.py
@@ -1,8 +1,27 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.db import models
 
 
-class Customer(AbstractBaseUser, PermissionsMixin):
+class Customer(AbstractUser):
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[AbstractUser.username_validator],
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+        null=True,
+        blank=True
+    )
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     class Meta:
         verbose_name = "Customer"
         verbose_name_plural = "Customer"
