8d7a7694098cf769285d3073dab3e31b5b2017d0
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-23 10:43:16 +0200

    Create model Customer

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customer"
