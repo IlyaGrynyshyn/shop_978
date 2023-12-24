670635e9fcd644e3825be0f12f3eb96aabc71e75
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-24 13:26:07 +0200

    Implement customer profile

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from account.views import CustomerRegistrationView
+from account.views import CustomerRegistrationView, ProfileDetailView
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(success_url="account:profile"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('registration/', CustomerRegistrationView.as_view(), name='registration')
+    path('registration/', CustomerRegistrationView.as_view(), name='registration'),
+    path('profile/', ProfileDetailView.as_view(), name='profile')
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView
 
 from account.forms import CustomerRegistrationForm
+from account.models import Customer
 
 
 class CustomerRegistrationView(FormView):
@@ -14,4 +16,15 @@ class CustomerRegistrationView(FormView):
         if form.is_valid():
             form.save(commit=True)
         return super().form_valid(form)
+
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model=Customer
+    template_name = 'account/profile.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user.username
+
+
+
 
