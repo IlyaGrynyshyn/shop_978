d8eae2d1b8a482a8e635e60211cf8eff0609971b
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-24 22:46:23 +0200

    Add to login & logout urls customer views

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from account.views import CustomerRegistrationView, ProfileDetailView
+from account.views import CustomerRegistrationView, ProfileDetailView, CustomerLoginView, logout_user
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(success_url="account:profile"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', CustomerLoginView.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('registration/', CustomerRegistrationView.as_view(), name='registration'),
     path('profile/', ProfileDetailView.as_view(), name='profile')
 ]
