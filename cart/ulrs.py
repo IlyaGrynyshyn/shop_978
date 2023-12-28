d1f8cd8c24390972e72ab669de2382fe0bc05f4b
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-28 14:34:04 +0200

    Add urls

--- cart/ulrs.py
+++ cart/ulrs.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from cart.views import CartDetailView, CartAddView, CartRemoveView, AddQuantityView, SubtractionQuantityView
 
+app_name = 'cart'
 
 urlpatterns = [
-
+    path('', CartDetailView.as_view(), name='cart_detail'),
+    path('add/<int:product_id>/', CartAddView.as_view(), name='cart_add'),
+    path('remove/<int:product_id>/', CartRemoveView.as_view(), name='cart_remove'),
+    path('add_qty/<int:product_id>/', AddQuantityView.as_view(), name='change_qty'),
+    path('subtraction_quantity/<int:product_id>/', SubtractionQuantityView.as_view(), name='subtraction_quantity')
 ]
