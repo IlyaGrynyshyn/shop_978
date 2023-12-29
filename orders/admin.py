4243a40762d0c6019906a0191b42815276def85b
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-29 17:12:30 +0200

    Implement admin settings for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', "created", "status", "paid"]
+    list_filter = ['id', "created", "status", "paid", "status"]
+    search_fields = ['id', 'last_name', "email", "phone", "customer"]
+    inlines = [OrderItemInline]
