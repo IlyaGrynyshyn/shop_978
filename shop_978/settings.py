94be1d6d56a2b38f1c57adb57949b551d8074f9f
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-12-30 17:57:39 +0200

    Add env

--- shop_978/settings.py
+++ shop_978/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w$!#)x-_&oh5h+3$r+g(i+wv9qq@8(k@v+!(6kbt!u8(@fu0^*"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
