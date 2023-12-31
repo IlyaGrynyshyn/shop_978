a5a571b9c903632e84ecdac4850f03dcf367e107
Ilya <38630970+IlyaGrynyshyn@users.noreply.github.com>
2023-12-31 17:56:09 +0200

    Delete mainapp directory

--- mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from mainapp.views import BaseListView, CategoryListView, ProductDetailView
-
-urlpatterns = [
-    path("", BaseListView.as_view(), name="home"),
-    path('product/<slug:product_slug>', ProductDetailView.as_view(), name='product_detail'),
-    path('<slug:top_category_slug>/<slug:category_slug>', CategoryListView.as_view(), name='category_detail')
-
-]
