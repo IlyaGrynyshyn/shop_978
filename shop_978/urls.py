a8a5f496cf431de6f01d8cf8fb43ee81b7dc5370
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-01 20:35:00 +0200

    Remove if debug for media files in urls.py

--- shop_978/urls.py
+++ shop_978/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
                   path("order/", include("orders.urls"))
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
