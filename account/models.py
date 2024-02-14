8e5906aae02e659f7c02192083fcd6acbe1b0d9f
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-14 13:05:40 +0200

    add new manager to Customer model and create migrations

--- account/models.py
+++ account/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
+from account.managers import UserManager
 
 
 class Customer(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
     phone = models.CharField(_('phone number'), max_length=30, null=True, blank=True, default="")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         verbose_name = _('customer')
